4847248980aa55bb16c2f91a5528bb8f84495baa
Brendon Yenson <brendon.yenson@propelleraero.com.au>
2025-12-03 09:46:30 +1100

    better loop implementation

--- day02/b.py
+++ day02/b.py
@@ -2,26 +2,28 @@ import sys
 
 
 def main():
+    def check(value):
+        rep = str(value)
+        length = len(rep)
+        for j in range(1, length):
+            if length % j == 0:  # we can repeat j characters
+                # take the first j characters
+                pattern = rep[:j]
+                remade = pattern * (length // j)
+                if remade == rep:
+                    return value
+        return 0
+
     def do(line):
-        invalids = []
+        invalids = 0
         ranges = line.split(",")
         for r in ranges:
             values = r.split("-")
             first: int = int(values[0])
             second: int = int(values[1])
             for i in range(first, second + 1):
-                rep = str(i)
-                length = len(rep)
-                for j in range(1, length):
-                    if length % j == 0:
-                        # take the first j characters
-                        pattern = rep[:j]
-                        remade = pattern * (length // j)
-                        if remade == rep:
-                            print(i)
-                            invalids.append(i)
-        print(sum(set(invalids)))  # convert to a set to remove double counts
-        # eg. 1111 is 11 11 and 1 1 1 1, but only count it once
+                invalids += check(i)
+        print(invalids)
 
     if len(sys.argv) == 1:
         with open("input.txt", "r") as f:
